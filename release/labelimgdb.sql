5edf2f4f0f0302556f236cd1bcbb7686fbc088ba
ShaunMermet <shaun.mermet@gmail.com>
2017-06-14 14:01:30 +0900

    Version 0.3.5

--- release/labelimgdb.sql
+++ release/labelimgdb.sql
@@ -855,3 +855,7 @@ ALTER TABLE `segimages` ADD `cprs_rate` INT NOT NULL DEFAULT '-1' COMMENT 'Rate 
 --------------------------------
 --- Changes for Beta 0.3.4 ----
 --------------------------------
+
+--------------------------------
+--- Changes for Beta 0.3.5 ----
+--------------------------------
